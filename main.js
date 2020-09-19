05ff2b773aadd5a1d79345a6db6027bb654450e5
Glodenox <github@tomputtemans.com>
2020-09-19 02:55:29 +0200

    Implement settings window #1

--- main.js
+++ main.js
@@ -1,19 +1,77 @@
-/* Inspirated by https://gist.github.com/AlcaDesign/742d8cb82e3e93ad4205 */
-
+/* Configuration */
 var options = {
 	connection: {
 		secure: true,
 		reconnect: true
 	},
-	channels: [ "loadingreadyrun" ]
+	channels: [ "emongg" ]
 };
 
 var chat = document.getElementById('chat'),
-	chatContainer = document.getElementById('chat-container');;
-var scrollDistance = 0, // How many pixels are we currently still hiding?
+	chatContainer = document.getElementById('chat-container'),
+	scrollDistance = 0, // How many pixels are we currently still hiding?
 	scrollReference = 0, // Distance when we started scrolling
-	scrollDuration = 1; // Time in seconds allowed for a message to appear
+	newMessagesOnTop = getConfig('new-messages-on-top', 'true') == 'true',
+	smoothScroll = getConfig('smooth-scroll', 'true') == 'true',
+	scrollDuration = parseInt(getConfig('smooth-scroll-duration', 1000)), // Time in milliseconds allowed for a message to appear
+	styles = { 'background-color': '', 'odd-background-color': '', 'separator-color': '', 'text-color': '', 'user-color': '', 'moderator-color': '' };
+Object.keys(styles).forEach(key => {
+	styles[key] = getConfig(key, getComputedStyle(document.body).getPropertyValue('--' + key).trim());
+	document.body.style.setProperty('--' + key, styles[key]);
+});
 
+/* Interface interactions */
+document.getElementById('settings-wheel').addEventListener('click', () => document.getElementById('settings').classList.toggle('hidden'));
+// Style
+Object.keys(styles).forEach(key => {
+	document.getElementById('settings-' + key).value = styles[key];
+	document.getElementById('settings-' + key).addEventListener('change', (e) => {
+		styles[key] = e.target.value;
+		document.body.style.setProperty('--' + key, e.target.value);
+		localStorage.setItem(key, e.target.value);
+	});
+});
+// Behavior
+document.body.classList.toggle('reverse-order', !newMessagesOnTop);
+document.getElementById('settings-new-messages-on-top').checked = newMessagesOnTop;
+document.getElementById('settings-new-messages-on-top').addEventListener('click', () => {
+	newMessagesOnTop = !newMessagesOnTop;
+	localStorage.setItem('new-messages-on-top', newMessagesOnTop);
+	document.body.classList.toggle('reverse-order', !newMessagesOnTop);
+	scrollDistance = scrollReference = 0;
+	chatContainer.scrollTop = newMessagesOnTop ? 0 : chatContainer.scrollTopMax;
+});
+document.getElementById('settings-smooth-scroll').checked = smoothScroll;
+document.getElementById('settings-smooth-scroll').addEventListener('click', () => {
+	smoothScroll = !smoothScroll;
+	localStorage.setItem('smooth-scroll', smoothScroll);
+	scrollDistance = scrollReference = 0;
+	chatContainer.scrollTop = newMessagesOnTop ? 0 : chatContainer.scrollTopMax;
+	document.getElementById('settings-smooth-scroll').parentNode.nextElementSibling.classList.toggle('hidden', !smoothScroll);
+});
+if (smoothScroll) {
+	document.getElementById('settings-smooth-scroll').parentNode.nextElementSibling.classList.remove('hidden');
+}
+document.getElementById('settings-smooth-scroll-duration').value = scrollDuration;
+document.getElementById('settings-smooth-scroll-duration').addEventListener('input', (e) => {
+	var duration = parseInt(e.target.value);
+	if (!isNaN(duration) && e.target.validity.valid) {
+		scrollDuration = duration;
+		localStorage.setItem('smooth-scroll-duration', scrollDuration);
+	}
+});
+
+document.body.addEventListener('keydown', (e) => {
+	if ((e.key == "H" || e.key == "h") && e.shiftKey && e.ctrlKey) {
+		document.getElementById('curtain').classList.toggle('hidden');
+	} else if ((e.key == "S" || e.key == "s") && e.shiftKey && e.ctrlKey) {
+		document.getElementById('settings').classList.toggle('hidden');
+	} else if ((e.key == "Escape")) {
+		document.getElementById('settings').classList.add('hidden');
+	}
+});
+
+/* Retrieve chat */
 var client = new tmi.client(options);
 client.addListener('message', handleChat);
 client.connect();
@@ -21,28 +79,33 @@ client.connect();
 // Continually scroll up, in a way to make the comments readable
 var lastFrame = +new Date();
 function scrollUp(now) {
-	if (scrollDistance > 0) {
+	if (smoothScroll && scrollDistance > 0) {
 		// Estimate how far along we are in scrolling in the current scroll reference
-		var currentStep = (scrollDuration * 1000) / (now - lastFrame);
+		var currentStep = scrollDuration / (now - lastFrame);
 		scrollDistance -= scrollReference / currentStep;
-		scrollDistance = Math.max(Math.floor(scrollDistance), 0);
-		chatContainer.scrollTop = scrollDistance;
+		scrollDistance = Math.max(scrollDistance, 0);
+		chatContainer.scrollTop = Math.round(newMessagesOnTop ? scrollDistance : chatContainer.scrollTopMax - scrollDistance);
 	}
 	lastFrame = now;
 	window.requestAnimationFrame(scrollUp);
 }
 window.requestAnimationFrame(scrollUp);
-chatContainer.scrollTop = 0;
 
+function getConfig(key, defaultValue) {
+	var item = localStorage.getItem(key);
+	return item === null ? defaultValue : item;
+}
+
+/* Inspirated by https://gist.github.com/AlcaDesign/742d8cb82e3e93ad4205 */
 function handleChat(channel, userstate, message, self) {
-	console.log(channel, userstate, message);
+	//console.log(channel, userstate, message);
 	var chatLine = document.createElement('div'),
 		chatName = document.createElement('span'),
 		chatColon = document.createElement('span'),
 		chatMessage = document.createElement('span');
 
 	// Fill chat line with content
-	chatName.className = 'chat-author';
+	chatName.className = 'chat-user';
 	if (userstate.mod) {
 		chatName.classList.add('moderator');
 	}
@@ -56,6 +119,9 @@ function handleChat(channel, userstate, message, self) {
 
 	// Calculate height for smooth scrolling
 	scrollReference = scrollDistance += chatLine.scrollHeight;
+	if (!newMessagesOnTop && !smoothScroll) {
+		chatContainer.scrollTop = chatContainer.scrollTopMax;
+	}
 
 	// Check whether we can remove the two oldest messages
 	if (chat.childNodes.length > 2 && window.innerHeight + scrollDistance < chat.scrollHeight - chat.firstChild.scrollHeight - chat.childNodes[1].scrollHeight) {
